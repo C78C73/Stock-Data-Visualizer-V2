b941cf8b6a131ee7e1c8e889f0d427902e721ee4
Caleb <ccwkkw@umsystem.edu>
2025-04-16 20:34:34 -0500

    Got it working with docker
    changed the code to test but putting it back next - Caleb

---
+++ StockDataVisualizer.py
@@ -0,0 +1,23 @@
+import json, requests, datetime, time, json
+#import matplotlib.pyplot as plt
+#import mplfinance as mpf
+#import pandas as pd
+import pygal
+import numpy as np
+from flask import Flask
+#import redis
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+    #variables
+    apiKey = "WZYK9TQ3C96A9WXT"
+
+    #Ask the user to enter the stock symbol for the company they want data for.
+    return f'AHHHHHHHHHHHHHHHHHHH ccwkkw\n'
+app.run(host="0.0.0.0")
+
+   
